abe6b443d67ea00d6e8e8349c914337648c5fa8d
MUSA-TOMIOKA <musasi6346241090@icloud.com>
2025-08-03 16:57:15 +0900

    プロトタイプ
    試作的に作成していた物をベースとする

---
+++ 電卓開発練習.cpp
@@ -0,0 +1,19 @@
+﻿#include <windows.h>
+//コンソールアプリケーション用メイン
+int main()
+{
+
+}
+
+
+
+//GUIアプリケーション用メイン
+int APIENTRY wWinMain
+(_In_ HINSTANCE hInstance,       //インスタンスハンドル(モジュールハンドル)
+_In_opt_ HINSTANCE hPrevInstance,//必ずnullになる()
+_In_ LPWSTR    lpCmdLine,
+_In_ int       nCmdShow
+)
+{
+	return 0; //0は正常終了,0以外はエラー
+}
